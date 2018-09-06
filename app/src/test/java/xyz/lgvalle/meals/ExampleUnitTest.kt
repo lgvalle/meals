43520630666f2520f495c9e4405f19cafd9d7982
lgvalle <luis.gvalle@gmail.com>
2018-09-06 15:52:17 +0100

    first commit

---
+++ app/src/test/java/xyz/lgvalle/meals/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package xyz.lgvalle.meals
+
+import org.fest.assertions.api.Assertions.assertThat
+import org.junit.Test
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertThat(2 + 2).isEqualTo(4)
+    }
+}
